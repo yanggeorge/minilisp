3f70bb5b3cba2024c9749b8b79496942c789b993
ym151925 <ym151925@alibaba-inc.com>
2018-09-11 19:06:03 +0800

    增加CellNodeUtil

--- src/main/java/com/threelambda/minilisp/core/CellNodeUtil.java
+++ src/main/java/com/threelambda/minilisp/core/CellNodeUtil.java
@@ -1,7 +1,9 @@
 package com.threelambda.minilisp.core;
 
 import com.threelambda.minilisp.node.CellNode;
+import com.threelambda.minilisp.node.Node;
 import com.threelambda.minilisp.node.SExprNode;
+import javafx.scene.control.Cell;
 
 /**
  * @author yangming 2018/8/29
@@ -10,10 +12,32 @@ public class CellNodeUtil {
 
     /**
      * 获取第一个cell的car下的SExpr
+     *
      * @param cellNode
      * @return
      */
     public static SExprNode getFirst(CellNode cellNode) {
+        if (cellNode != null) {
+            return (SExprNode)cellNode.car;
+        }
+        return null;
+    }
+
+    /**
+     * 返回下一个cell
+     *
+     * @param cellNode
+     * @return
+     */
+    public static CellNode nextCell(CellNode cellNode) {
+        if (cellNode != null) {
+            Node cdr = cellNode.cdr;
+            if (cdr instanceof CellNode) {
+                return (CellNode)cdr;
+            } else if (cdr instanceof SExprNode) {
+                return null;
+            }
+        }
 
         return null;
     }
@@ -21,19 +45,23 @@ public class CellNodeUtil {
     /**
      * 获取最后一个cell的car下的SExpr
      * 如果最后一个cell为nil则car是null
+     *
      * @param cellNode
      * @return
      */
-    public static SExprNode getLast(CellNode cellNode) {
-        return null;
+    public static SExprNode getLastCar(CellNode cellNode) {
+        CellNode lastCell = getLastCell(cellNode);
+        return getFirst(lastCell);
     }
 
-    /**
-     * 返回下一个cell
-     * @param cellNode
-     * @return
-     */
-    public static CellNode nextCell(CellNode cellNode) {
-        return null;
+
+    public static CellNode getLastCell(CellNode cellNode) {
+        CellNode curr = cellNode;
+        CellNode next = nextCell(curr);
+        while (next != null) {
+            curr = next;
+            next = nextCell(curr);
+        }
+        return curr;
     }
 }
